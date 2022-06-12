9fd24811501f34b777904b9f777932c666c43c04
Polobear PTF <82516345+Polobear9@users.noreply.github.com>
2022-06-12 18:05:48 +0900

    Add files via upload

--- Index.java
+++ Index.java
@@ -1,37 +1,43 @@
 import java.util.Random;
 import java.util.Scanner;
 
+
+//Make a info of number and source
 public class Index {
 	int UserAnswer;
 	int counter = 0;
 	int life = 5;
 	
 	public static void main(String[] args) {
+		
+		//Random class is help to make a random number.
 		Random ran = new Random();
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		ComputerNum cn = new ComputerNum(ran.nextInt(100));
 		Index ua = new Index();
 		
-		// TODO Auto-generated method stub
+		//when game is over >>> life == 0
 		while(ua.UserAnswer != cn.Answer) {
 			if(ua.life == 0) {
 				System.out.println("The life is Zero... Game is over");
 				break;
 			}
 			
-			
+			//help to how many times user answer.
 			System.out.println("Now you Try : " + ua.counter + " Times");
 			System.out.println("Tell me the answer");
 			int AN = sc.nextInt();
 			ua.UserAnswer = AN;
 			
+			//help to find a position a number.
 			if(AN > cn.Answer) {
 				System.out.println("The Number is Lower Then Your Answer");
 			}else {
 				System.out.println("The Number is higher Then Your Answer");
 			}
 
+			//Check a User Answer input value for correct
 			System.out.println("You Answer is : " + AN);
 			if(ua.UserAnswer == cn.Answer) {
 				System.out.println("correct!");
